8321757c3722500746f9582e30a174e4d9a7c0e6
Snorlax104 <damiano.prette@gmail.com>
2026-04-08 01:01:35 +0200

    Add files via upload

--- lega.cpp
+++ lega.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Player{
@@ -7,18 +8,24 @@ class Player{
     string name;
     int credits, points;
 
+    //affect the player when gather points by reducing or increasing the points of the player
+    //might even istantly add or take points from the player
+    vector<int> bonusmalus;
+
 public:
    
-    Player(string n, int c, int p) {
-        
-        cout << "inserire il nome del giocatore: " << endl;
-        cin >> name;
-        
-        credits = 100;
-        points = 0;
-    }
-
+    Player(string n, int c, int p) : name(n), credits(c), points(p) {}
     int GetPoints() const;
+
+    void modifyPoints(int p);
+    void addBonusMalus(int bm);
+
+};
+
+class Quest {
+
+    //class where you can put some quests to assign to the players
+    //with some specific methods we can put custom quests or even update quest regularly
 
 };
 
@@ -26,22 +33,56 @@ class Lega {
 
     string name;
     vector<Player> players;
-    vector<int> index;
+
+    Quest quests;
 
 public:
 
-    Lega(string n) {
+    Lega() {
 
         cout << "inserire il nome della lega: " << endl;
         cin >> name;
 
+        //temporary way to add players. we nead to upgrade this part once we'll use files as mass memory
+        cout << "inserire il numero di giocatori: " << endl;
+        int numPlayers;
+        cin >> numPlayers;
+
+            for (int i = 0; i < numPlayers; ++i) {
+    
+                string playerName;
+                int credits, points;
+    
+                cout << "inserire il nome del giocatore: " << endl;
+                cin >> playerName;
+    
+                cout << "inserire i crediti del giocatore: " << endl;
+                cin >> credits;
+    
+                cout << "inserire i punti del giocatore: " << endl;
+                cin >> points;
+    
+                players.emplace_back(playerName, credits, points);
+    
+            }
+
     }
 
-    void gestioneClassifica(const vector<Player>& players, vector<int>& index);
+    void gestioneClassifica();
 
 };
 
+void asta() {
+
+    //get from the extern all the stdents/professors to create your team
+
+}
+
 int main() {
+
+    Lega lega;
+
+    lega.gestioneClassifica();
     
     return 0;
 }
@@ -52,45 +93,25 @@ int Player::GetPoints() const {
 
 }
 
-void Lega::gestioneClassifica(const vector<Player>& players, vector<int>& index) {
+void Player::modifyPoints(int p){
 
-    for (int i = 0; i < players.size(); i++) {
-        
-        if (players.size() == 0) {
-            
-            for (int f = 0; f < players.size(); f++) {
+    //used to change the points of each player due to the quests points affected by bonus and malus
+    points += p;
 
-                if (f == 0) {
+}
 
-                    index.push_back(f);
+void Player::addBonusMalus(int bm){
 
-                }
-                else {
+    bonusmalus.push_back(bm);
 
-                    if(players[f].GetPoints() > players[f-1].GetPoints()){
+}
 
-                        int a = index[f-1];
-                        index[f-1] = f;
-                        index.push_back(a);
+void Lega::gestioneClassifica() {
 
-                    }
-                    else {
+    sort(players.begin(), players.end(), [](const Player& a, const Player& b) {
 
-                        index.push_back(f);
+        return a.GetPoints() > b.GetPoints();
 
-                    }
-
-                }
-
-            }
-
-        }
-        else {
-                
-            
-
-        }
-
-    }
+    });
 
 }
