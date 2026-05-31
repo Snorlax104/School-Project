b6357073d3b12c36f8709efd45984af7333030f2
Divan264 <donati.ivan2008@gmail.com>
2026-05-31 11:32:03 +0200

    Update Leag-Player.h

--- final project/classes/gruppo2/Leag-Player.h
+++ final project/classes/gruppo2/Leag-Player.h
@@ -29,6 +29,10 @@ namespace __FANTASCUOLA_G2_LEAG__ {
     
         Player() {} //constructor to create a player with name, credits and points
         //should get the parameters from a memory organization method
+
+         Player(string name, int credits, int points) //Serve quando si ricarica dal JSON: il Serializer legge nome, crediti e punti dal file e ricostruisce un Player con quei valori. 
+        : _name(name), _credits(credits), _points(points) {}
+
         ~Player() {} //destructor
         
         int GetPoints() const;
@@ -38,6 +42,9 @@ namespace __FANTASCUOLA_G2_LEAG__ {
         void modifyCredits(int c); //to modify the credits of the player, to be used in the auction phase
         void addBonusMalus(int bm);
 
+         string GetName() const { return _name; } //restituisce il nome del player
+         const Team& getTeam() const { return _team; } //restituisce il nome del team
+        
         friend void asta(const vector<Player>& players);
 
     };
